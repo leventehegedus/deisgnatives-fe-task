564716c85a4979b43118f32799850a915e7432c2
Levente Hegedüs <levi.hegedus92@gmail.com>
2024-02-27 02:04:33 +0100

    slider added, slider fixes

--- designatives-task/src/app/components/References/References.tsx
+++ designatives-task/src/app/components/References/References.tsx
@@ -1,8 +1,25 @@
+"use client";
 import { IReference } from "@/app/types";
 import Reference from "../Reference/Reference";
 import "./References.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import React, { useRef } from "react";
+import Image from "next/image";
+
+const settings = {
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+};
 
 const References: React.FC = () => {
+  const slider = useRef<Slider>(null);
+
   const references: IReference[] = [
     {
       title: "Budaörs",
@@ -10,13 +27,13 @@ const References: React.FC = () => {
       img: "/ref_1.png",
     },
     {
-      title: "Budaörs",
-      description: "Decathlon",
+      title: "Üllő",
+      description: "Teljesen automata zöldfal",
       img: "/ref_2.png",
     },
     {
-      title: "Budaörs",
-      description: "Decathlon",
+      title: "Balatonszántód",
+      description: "Balalaland",
       img: "/ref_3.png",
     },
   ];
@@ -25,9 +42,27 @@ const References: React.FC = () => {
     <div className="references">
       <div className="title">Referenciák</div>
       <div className="slider">
-        {references.map((ref: IReference, index) => (
-          <Reference reference={ref} key={index} />
-        ))}
+        <Slider ref={slider} {...settings}>
+          {references.map((ref: IReference, index) => (
+            <Reference reference={ref} key={index} />
+          ))}
+        </Slider>
+        <div className="desktop-view">
+          <div className="custom-slider-buttons">
+            <button
+              className="prev"
+              onClick={() => slider?.current?.slickPrev()}
+            >
+              <Image src="/arrow.svg" alt="Arrow" width={18} height={18} />
+            </button>
+            <button
+              className="next"
+              onClick={() => slider?.current?.slickNext()}
+            >
+              <Image src="/arrow.svg" alt="Arrow" width={18} height={18} />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
